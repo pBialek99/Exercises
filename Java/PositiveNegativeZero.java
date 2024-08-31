96112a7ed8d1b6c28b45d6e00d9bb313105ae16f
pBialek99 <pawel99bialek@gmail.com>
2024-08-31 16:18:33 +0200

    Create PositiveNegativeZero.java

---
+++ Java/PositiveNegativeZero.java
@@ -0,0 +1,8 @@
+public class PositiveNegativeZero {
+    // write code here
+    public static void checkNumber(int number) {
+        if (number > 0) System.out.println("positive");
+        if (number < 0) System.out.println("negative");
+        if (number == 0) System.out.println("zero");
+    }
+}
