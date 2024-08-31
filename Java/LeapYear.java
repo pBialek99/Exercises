5a46133ba2216098b6aa3b86471d4a6a98233baa
pBialek99 <pawel99bialek@gmail.com>
2024-08-31 17:33:21 +0200

    Create LeapYear.java

---
+++ Java/LeapYear.java
@@ -0,0 +1,6 @@
+public class LeapYear {
+    // write code here
+    public static boolean isLeapYear(int year) {
+        return (year >= 1 && year <= 9999) ? ((year % 4 == 0) ? ((year % 100 != 0) || (year % 400 == 0)) : false) : false;
+    }
+}
