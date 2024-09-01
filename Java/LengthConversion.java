b7f4d404679d2ed2bda4fb79fc3cc2ecb0477acb
pBialek99 <pawel99bialek@gmail.com>
2024-09-01 17:59:14 +0200

    Update LengthConversion.java

--- Java/LengthConversion.java
+++ Java/LengthConversion.java
@@ -1,3 +1,10 @@
-public static void LengthConversion() {
-  public static void 
+public class LengthConversion {
+  
+  public static double convertToCentimeters(int height) {
+    return (double) (height * 2.54d);
+  } 
+
+  public static double convertToCentimeters(int feet, int inches) {
+    return (double) ((feet * 12 + inches) * 2.54d);
+  }
 }
