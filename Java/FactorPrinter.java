9f82195c893ce608c97661731ac3a79b20f66c0e
pBialek99 <pawel99bialek@gmail.com>
2024-09-03 23:24:03 +0200

    Create FactorPrinter.java

---
+++ Java/FactorPrinter.java
@@ -0,0 +1,17 @@
+public class FactorPrinter {
+    // write code here
+    public static void printFactors(int number) {
+        if (number >= 1) { 
+            int i = 1;
+            while (i <= number) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+                i++;
+            }
+        }
+        else {
+            System.out.println("Invalid Value");
+        }    
+    }
+}
